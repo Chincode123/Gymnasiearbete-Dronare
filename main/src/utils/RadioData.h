265794238d4bbcca207feaad7997ec67b50325d7
Noah <noah.westerberg2@elev.ga.ntig.se>
2024-12-11 13:26:59 +0100

    convert values to byte instructions

--- main/src/utils/RadioData.h
+++ main/src/utils/RadioData.h
@@ -3,20 +3,60 @@
 
 #define PIPE_ADDRESSES {"00001", "00002"}
 
-#define BUTTON_SOUTH    0b00000001
-#define BUTTON_WEST     0b00000010
-#define BUTTON_NORTH    0b00000100
-#define BUTTON_EAST     0b00001000
+#define BUTTON_0    0b00000001
+#define BUTTON_1    0b00000010
+#define BUTTON_2    0b00000100
+#define BUTTON_3    0b00001000
+#define BUTTON_4    0b00010000
+#define BUTTON_5    0b00100000
+#define BUTTON_6    0b01000000
+#define BUTTON_7    0b10000000
+
+#define _MSG_CONTROLLER_INPUT 0
+#define _MSG_SET_PID_V 1
+#define _MSG_SET_PID_P 2
+#define _MSG_SET_PID_R 3
+
+// message length in bytes
+#define _MSG_LENGHT_0 4
+#define _MSG_LENGHT_1 12
+#define _MSG_LENGHT_2 12
+#define _MSG_LENGHT_3 12
 
 struct controllerInstructions {
-    // om det i framtiden implementeras att ge direkta positions instructionen för att flytta på drönaren hade det varit i en annan struct
-    uint8_t msg_type; 
-
+    // devide by 127
     int8_t
         stick_LX, stick_LY,
-        triggerValues;
+        power;
 
     uint8_t button_input;
+};
+
+struct PID_Instructions {
+    float k_p, k_i, k_d;
+};
+
+struct gyroscopeValues {
+    float   pitch,
+            roll,
+            yaw;
+
+    float   accX,
+            accY,
+            accZ;
+};
+
+struct gyroscopeErrors{
+    float   accErrorX, 
+            accErrorY, 
+            gyroErrorX, 
+            gyroErrorY, 
+            gyroErrorZ;
+};
+
+struct gyroscopeData{
+    gyroscopeValues values;
+    gyroscopeError errors;
 };
 
 struct droneInfo {
--- test/web-serial-communication-test/web/main.js
+++ test/web-serial-communication-test/web/main.js
@@ -1,6 +1,138 @@
 let port;
 
-document.querySelector("button").addEventListener('click', async () => {
+document.querySelector("#select-serial").addEventListener("click", async () => {
     port = await navigator.serial.requestPort();
-    console.log(port);
+    setInterval(read, 10);
 });
+
+let writing = false
+
+write = async (data) => {
+    try {
+        const writer = port.writable.getWriter();
+    }
+    catch (error){
+        console.log(error);
+    }
+
+    await writer.write(data);
+
+    // Allow the serial port to be closed later.
+    writer.releaseLock();
+};
+
+read = async () => {
+    
+    while (port.readable) {
+        const reader = port.readable.getReader();
+      
+        try {
+          while (true) {
+
+            const { value, done } = await reader.read();
+
+            if (done) {
+              // Allow the serial port to be closed later.
+              document.getElementById('output').innerHTML += "<br>";
+              reader.releaseLock();
+              break;
+            }
+            if (value) {
+                console.log(value);
+                document.getElementById('output').innerHTML += new TextDecoder().decode(value);
+            }
+          }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+};
+
+document.getElementById("open").addEventListener('click', async () => {
+    try {
+        await port.close();
+    }
+    catch (error) {
+        console.log(error);
+    }
+    
+    await port.open({baudRate: 9600});
+});
+
+class buttonType {
+    activated;
+    value;
+};
+
+const buttons = new Array(8);
+// populate
+let buttonActivationValue = 1;
+for (let i = 0; i < 8; i++){
+    buttons[i] = new buttonType();
+    buttons[i].value = buttonActivationValue;
+    buttons[i].activated = false;
+    buttonActivationValue = buttonActivationValue << 1;
+}
+
+switchButtonActivation = (index) => {
+    console.log("set: " + index);
+    buttons[index].activated = !buttons[index].activated;
+};
+
+// -1 <= x, y, power <= 1
+getControllerInstructions = (x, y, power) => {
+    const values = new Int8Array(3);
+    values[0] = (x * 127) & 0x0000FF;
+    values[1] = (y * 127) & 0x0000FF;
+    values[2] = (power * 127) & 0x0000FF;
+
+    let buttonValues;
+    buttons.forEach(button => {
+        if (button.activated){
+            buttonValues = buttonValues | button.value;
+        }
+    });
+
+    const messageType = 0;
+    out = new Uint8Array(6)
+    out[0] = messageType;
+    out[1] = values[0];
+    out[2] = values[1];
+    out[3] = values[2];
+    out[4] = buttonValues;
+    out[5] = messageType;
+
+    return out;
+};
+
+getPIDInstructions = (p, i, d, module) => {
+    const floatValues = new Float32Array(3);
+    floatValues[0] = p;
+    floatValues[1] = i;
+    floatValues[2] = d;
+
+    const byteValues = new Uint8Array(floatValues.buffer);
+
+    let messageType;
+    if (module == "velocity"){
+        messageType = 1
+    } 
+    else if (module == "pitch") {
+        messageType = 2
+    }
+    else if (module == "roll") {
+        messageType = 3
+    }
+    else {
+        throw {name: "PID-ModuleError", message:"PID moudule type does not exist"};
+    }
+
+    out = new Uint8Array(14);
+    out[0] = messageType;
+    out[13] = messageType;
+    for (let i = 1; i < 13; i++){
+        out[i] = byteValues[i-1];
+    }
+    return out;
+}
