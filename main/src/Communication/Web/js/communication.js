4810dd9505337b4df8ae605b1dafb22ed89323a6
Noah <noah.westerberg.nw@gmail.com>
2025-01-27 00:54:13 +0100

    Increas terminal font-size

--- main/src/Communication/Web/js/communication.js
+++ main/src/Communication/Web/js/communication.js
@@ -336,7 +336,7 @@ addToTerminal = (log) => {
     return;
   }
 
-  output.innerHTML = `<div><span>1</span><span> | </span>${log}</div>` + output.innerHTML;
+  output.innerHTML = `<div><span>1</span> | ${log}</div>` + output.innerHTML;
   previousLog = log;
   if (output.children.length > 40) {
     output.removeChild(output.lastChild);
