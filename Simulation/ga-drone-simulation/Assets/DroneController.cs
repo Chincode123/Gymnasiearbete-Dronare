8b23010793f54b1a553ed2efc0f61ab16ed50b3f
Noah <noah.westerberg2@elev.ga.ntig.se>
2025-03-05 01:07:33 +0100

    Clean up simulation code

--- Simulation/ga-drone-simulation/Assets/DroneController.cs
+++ Simulation/ga-drone-simulation/Assets/DroneController.cs
@@ -6,39 +6,61 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
+    [Serializable] struct PIDConstants
+    {
+        public float P;
+        public float I;
+        public float D;
+
+        public PIDConstants(float p, float i, float d)
+        {
+            P = p;
+            I = i;
+            D = d;
+        }
+    }
+
     Vector3 acceleration, gyro, d_gyro;
     Vector3 previousVelocity, previousGyro;
 
+    [Header("Propeller Info")]
     [SerializeField] float maxPropellerPower;
     [SerializeField] float[] propellerPower = new float[4];
     [SerializeField] Transform[] propellers = new Transform[4];
     [SerializeField] Vector3[] propellerDirections = new Vector3[4];
-
     Vector3[] previousPorpellerPositions = new Vector3[4];
     Vector3[] propellerVelocities = new Vector3[4];
     Vector3[] previousPropellerVelocities = new Vector3[4];
     Vector3[] propellerAccelerations = new Vector3[4];
 
-
-    [SerializeField] float pitchP, pitchI, pitchD;
-    float pitchIntegral = 0, previousPitchError = 0;
+    [Header("Pitch")]
     [SerializeField] float desiredPitch;
+    [SerializeField] PIDConstants pitchPID;
+    float pitchIntegral = 0, previousPitchError = 0;
 
-    [SerializeField] float rollP, rollI, rollD;
-    float rollIntegral = 0, previousRollError = 0;
+    [Header("Roll")]
     [SerializeField] float desiredRoll;
+    [SerializeField] PIDConstants rollPID;
+    float rollIntegral = 0, previousRollError = 0;
 
-    [SerializeField] float velocityP, velocityI, velocityD;
-    float velocityIntegral = 0, previousVelocityError = 0;
+    [Header("Velocity")]
     [SerializeField] float desiredVelocity;
+    [SerializeField] PIDConstants velocityPID;
+    float velocityIntegral = 0, previousVelocityError = 0;
 
-    [SerializeField] float angleP, angleI, angleD;
 
-    [SerializeField] float velocityRange, pitchRange, rollRange;
-    [SerializeField] float currentPitch, currentRoll;
+    [Header("Ranges")]
+    [SerializeField] float velocityRange;
+    [SerializeField] float pitchRange;
+    [SerializeField] float rollRange;
+
+    [Header("Info")]
+    [SerializeField] float currentPitch;
+    [SerializeField] float currentRoll;
 
     Vector3 rotation;
 
+    [Header("References")]
     [SerializeField] new Rigidbody rigidbody;
 
     private void Start()
@@ -47,9 +69,6 @@ public class DroneController : MonoBehaviour
         {
             propellerDirections[i] = propellers[i].position - transform.position;
         }
-
-
-        //Time.timeScale = 0.1f;
     }
 
     void Update()
@@ -58,112 +77,52 @@ public class DroneController : MonoBehaviour
         desiredPitch = -pitchRange * Input.GetAxisRaw("Vertical");
         desiredRoll = -rollRange * Input.GetAxisRaw("Horizontal");
 
-        // currentPitch = Mathf.Atan2(-transform.up.z, transform.up.y) * Mathf.Rad2Deg;
-        // currentRoll = Mathf.Atan2(-transform.up.x, transform.up.y)  * Mathf.Rad2Deg;
-
         currentPitch = rigidbody.rotation.eulerAngles.x;
         currentRoll = rigidbody.rotation.eulerAngles.z;
         // Adjust angles to -180 to 180 range
         currentPitch = currentPitch > 180 ? -(currentPitch - 360) : -currentPitch;
         currentRoll = currentRoll > 180 ? currentRoll - 360 : currentRoll;
 
-
         ShiftPropellerPower();
-
     }
 
     void FixedUpdate()
     {
-
         for (int i = 0; i < 4; i++)
         {
                                                         // -0.5 <= (power / 255) <= 0.5
             rigidbody.AddForceAtPosition(transform.up * calculateMotorPower((propellerPower[i] / 255) + 0.5f), propellers[i].position);
-        }
-
-        UpdateGyroscopeValues(Time.fixedDeltaTime);
-    }
-
-
-    // [Get from mpu]
-    void UpdateGyroscopeValues(float timeIncrement)
-    {
-        acceleration = (rigidbody.velocity - previousVelocity) / timeIncrement;
-        previousVelocity = rigidbody.velocity;
-
-        gyro = rigidbody.angularVelocity * Mathf.Rad2Deg;
-        rotation += gyro * timeIncrement;
-        d_gyro = (gyro - previousGyro) / timeIncrement;
-        previousGyro = gyro;
-
-
-        // propelers
-        for (int i = 0; i < 4; i++)
-        {
-            propellerVelocities[i] = (propellers[i].position - previousPorpellerPositions[i]) / timeIncrement;
-            previousPorpellerPositions[i] = propellers[i].position;
-
-            propellerAccelerations[i] = (propellerVelocities[i] - previousPropellerVelocities[i]) / timeIncrement;
-            previousPropellerVelocities[i] = propellerVelocities[i];
-
-            //// velocities
-            //Debug.DrawLine(propellers[i].position, propellers[i].position + propellerVelocities[i], Color.blue);
-            //// accelerations
-            //Debug.DrawLine(propellers[i].position, propellers[i].position + propellerAccelerations[i], Color.red);
         }
     }
 
     void ShiftPropellerPower()
     {
         float velocityErorr = desiredVelocity - rigidbody.velocity.y;
-
         velocityIntegral += velocityErorr * Time.deltaTime;
-
         float velocityDerivitive = (velocityErorr - previousVelocityError) / Time.deltaTime;
         previousVelocityError = velocityErorr;
-
-        float basePower = velocityP * velocityErorr + velocityI * velocityIntegral + velocityD * velocityDerivitive;
+        float basePower = velocityPID.P * velocityErorr + velocityPID.I * velocityIntegral + velocityPID.D * velocityDerivitive;
         basePower = Mathf.Clamp(basePower, -127, 127);
 
         float pitchError = desiredPitch - currentPitch;
-
         pitchIntegral += pitchError * Time.deltaTime;
-
         float pitchDerivitive = (pitchError - previousPitchError) / Time.deltaTime;
         previousPitchError = pitchError;
-
-
-
-        float pitchShift = pitchP * pitchError + pitchI * pitchIntegral + pitchD * pitchDerivitive;
-
-
+        float pitchShift = pitchPID.P * pitchError + pitchPID.I * pitchIntegral + pitchPID.D * pitchDerivitive;
 
         float rollError = desiredRoll - currentRoll;
-
         pitchIntegral += rollError * Time.deltaTime;
-
         float rollDerivitive = (rollError - previousRollError) / Time.deltaTime;
         previousRollError = rollError;
-
-        float rollShift = rollP * rollError + rollI * rollIntegral + rollD * rollDerivitive;
-
-
+        float rollShift = rollPID.P * rollError + rollPID.I * rollIntegral + rollPID.D * rollDerivitive;
 
         propellerPower[0] = basePower + pitchShift + rollShift;
         propellerPower[1] = basePower + pitchShift - rollShift;
         propellerPower[2] = basePower - pitchShift - rollShift;
         propellerPower[3] = basePower - pitchShift + rollShift;
 
-
         for (int i = 0; i < 4; i++)
-        {
             propellerPower[i] = Mathf.Clamp(propellerPower[i], -127, 127);
-        }
-    }
-
-    float map(float x, float min, float max, float nMin, float nMax)
-    {
-        return (x / (max - min)) * (nMax - nMin);
     }
 
     // 0 <= x <= 1
